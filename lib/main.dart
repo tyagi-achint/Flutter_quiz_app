681c9a03538fe854cfd496c3fcc376014494bd08
tyagi-achint <achinttyagi001@gmail.com>
2024-08-11 11:31:24 +0530

    working

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,132 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(QuizzApp());
+
+class QuizzApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorecard = [];
+  List<String> questions = [
+    'Flutter is a UI toolkit developed by Apple.',
+    'Flutter uses the Dart programming language.',
+    'In Flutter, widgets are immutable.',
+    'StatefulWidget is used when the UI changes dynamically.',
+    'Flutter can only be used to develop Android apps.',
+    'The main function is the entry point of a Flutter application.',
+    'Hot Reload in Flutter allows you to apply changes without losing the application state.',
+    'In Flutter, `setState` is used to update the UI by rebuilding the widget.',
+    'Flutter applications cannot run on web browsers.',
+    'Flutter provides a rich set of Material Design and Cupertino widgets.',
+  ];
+
+  int questionNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions[questionNumber],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.green),
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  questionNumber++;
+                  scorecard.add(
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ),
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.red),
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  questionNumber++;
+
+                  scorecard.add(
+                    Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: scorecard,
+          ),
+        ),
+      ],
+    );
+  }
+}
